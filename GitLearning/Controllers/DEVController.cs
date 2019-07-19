d946998c6c549253d105441a0ae8b7f2381bb8da
amarpatil1 <amar.patil.1@springernature.com>
2019-07-19 11:43:25 +0530

    pushing code to dev branch

---
+++ GitLearning/Controllers/DEVController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitLearning.Controllers
+{
+    public class DEVController : Controller
+    {
+        public IActionResult Index()
+        {
+           // Dev Brach code
+           //Everything written here is only for tetsing 
+           //Kovali hi ratra rani
+           //Ain weli jagali
+           // marawa hi sampla ani saanj weli bhagali
+        
+            return View();
+        }
+    }
+}
